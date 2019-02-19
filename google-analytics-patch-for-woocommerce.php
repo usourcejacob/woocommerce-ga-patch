cb47c4992ca91773645a5fb0f5446291dbbc77f5
Jacob Hernandez <46951785+usourcejacob@users.noreply.github.com>
2019-02-19 11:21:58 +0800

    Initial release

--- google-analytics-patch-for-woocommerce.php
+++ google-analytics-patch-for-woocommerce.php
@@ -8,14 +8,10 @@
 * Author URI: https://usource.me/
 **/
 
-function fix_double_google_analytics () {
+function patch_woo_google_analytics () {
 	if ( ! is_admin() && class_exists( 'WC_Google_Analytics_JS' ) ) {
-		// Remove analytics.js script from WooCommerce Google Analytics Integration.
 		add_filter( 'woocommerce_ga_snippet_output', '__return_empty_string' );
-		// Always add universal_analytics_footer from WooCommerce Google Analytics Integration.
-		// Prevent if not is an WC page the ecommerce data is not send with ga( 'send', 'pageview' )
 		add_action( 'wp_footer', array( 'WC_Google_Analytics_JS', 'universal_analytics_footer' ) );
-		// Replace footer pageview with an event.
 		function replace_pageview_to_event ($js) {
 			return str_replace(
 				"ga( 'send', 'pageview' );",
@@ -26,4 +22,4 @@ function fix_double_google_analytics () {
 		add_filter( 'woocommerce_queued_js', 'replace_pageview_to_event' );
 	}
 }
-add_action( 'init', 'fix_double_google_analytics' );
+add_action( 'init', 'patch_woo_google_analytics' );
